28ae74b3cbe996d32fdce51c4eef169295175973
Ahmet Kadayıfçı <ahmetkadayfc@hotmail.com>
2021-08-22 19:50:51 +0300

    Project completed

--- content/js/menu.js
+++ content/js/menu.js
@@ -6,17 +6,3 @@ $(window).scroll(function() {
     $(".main_h .container-fluid").removeClass("sticky");
   }
 });
-
-// Navigation Scroll - ljepo radi materem
-// $("nav a").click(function(event) {
-//   var id = $(this).attr("href");
-//   var offset = 70;
-//   var target = $(id).offset().top - offset;
-//   $("html, body").animate(
-//     {
-//       scrollTop: target
-//     },
-//     500
-//   );
-//   event.preventDefault();
-// });
